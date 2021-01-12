9c64fa49581f15c60a8c2868c586c69f8accba27
Daniel Cepeda <jdcmjdcmjdcm@gmail.com>
2021-01-12 16:44:56 +0100

    inicia el commit prro

---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent{
+
+  constructor(private router:Router) {
+
+   }
+
+  ngOnInit(): void {
+  }
+  buscarHeroe(termino:string){
+      this.router.navigate(['/buscar', termino]);
+  }
+}
